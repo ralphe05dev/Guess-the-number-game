8f659744a03b3660bfd871808d47b759c7d7c6a7
ralphe05dev <raphaelodeareduo71@gmail.com>
2024-02-18 19:12:26 +0100

    Refactored code to implement DRY principle.

--- script.js
+++ script.js
@@ -8,17 +8,21 @@ let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let gameScore = 20;
 let highScore = 0;
 
+const displayMessage = message => {
+  messageText.textContent = message;
+};
+
 checkBtn.addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   // When there is no input
   if (!guess) {
-    messageText.textContent = '⛔ No number!';
+    displayMessage('⛔ No number');
 
     //When player wins
   } else if (guess === randomNumber) {
     numberBox.textContent = randomNumber;
-    messageText.textContent = '🎉 Correct number!';
+    displayMessage('💥 Correct number');
     document.body.style.backgroundColor = 'green';
     document.querySelector('.number').style.width = '30rem';
 
@@ -27,27 +31,14 @@ checkBtn.addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highScore;
     }
 
-    // When guess is higher than the random number
-  } else if (guess > randomNumber) {
+    // When guess is wrong
+  } else if (guess !== randomNumber) {
     if (gameScore > 1) {
-      messageText.textContent = '❗ Too high';
+      displayMessage(guess > randomNumber ? '❗ Too high' : '❗ Too low');
       gameScore--;
       document.querySelector('.score').textContent = gameScore;
     } else {
-      messageText.textContent = 'You lost the game❗';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    // When guess is lower than the random number
-  } else if (guess < randomNumber) {
-    if (gameScore > 1) {
-      messageText.textContent = '❗ Too low';
-      gameScore--;
-      document.querySelector('.score').textContent = gameScore;
-
-      // when the players tries a certain number of times and loses.
-    } else {
-      messageText.textContent = 'You lost the game❗';
+      displayMessage('You lost the game ❗');
       document.querySelector('.score').textContent = 0;
       document.body.style.backgroundColor = 'red';
     }
@@ -58,7 +49,7 @@ const resetBtn = document.querySelector('.again');
 resetBtn.addEventListener('click', function () {
   gameScore = 20;
   randomNumber = Math.trunc(Math.random() * 20) + 1;
-  messageText.textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   numberBox.textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('.score').textContent = gameScore;
